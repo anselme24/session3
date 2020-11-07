908071726698c00a927905351c60d4fd354e3a7a
anselme24 <abasigayabo@gmail.com>
2020-11-07 14:12:00 -0600

    3commit

--- src/main/java/VariousConcepts/Githubssetio3.java
+++ src/main/java/VariousConcepts/Githubssetio3.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
 	System.out.println(" git prontin 1");
 	System.out.println(" git prontin 2");
 	System.out.println(" git prontin 3");
-	System.out.println(" git prontin 4");
+	System.out.println(" git prontin 5");
 }
 }
